7cbe4a58c888b855765e4ff410cfaceb6f8a5ac1
Michael Martisnen <michael@cintix.dk>
2026-05-05 21:28:44 +0200

    build

---
+++ src/dk/cintix/application/server/Main.java
@@ -0,0 +1,8 @@
+package dk.cintix.application.server;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("cintix-application-server library module. No standalone runtime is configured.");
+    }
+}
