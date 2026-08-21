3b32644258897c7b2e7ea135a021e6318e5f7cf9
Michael Martisnen <michael@cintix.dk>
2026-08-21 13:30:41 +0200

    release: v3.3.0 — Security and hardning of GraphQL

--- src/dk/cintix/application/server/modules/http/server/services/domain/models/Response.java
+++ src/dk/cintix/application/server/modules/http/server/services/domain/models/Response.java
@@ -237,7 +237,7 @@ public class Response {
         response += "Date: " + dateFormat.format(new Date()) + "\r\n";
 
         if (!header.containsKey("Server")) {
-            response += "Server: Cintix-Application-Server(CAS)/3.2.1\r\n";
+            response += "Server: Cintix-Application-Server(CAS)/3.3\r\n";
         }
 
         for (String key : header.keySet()) {
